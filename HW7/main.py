88b92fc249f00f4eef724d6af4b7648391337def
Eugen <eugen.ganta@gmail.com>
2022-02-22 13:50:06 +0200

    HW

--- HW7/main.py
+++ HW7/main.py
@@ -1,29 +1,40 @@
 import my_library.my_library
 
-
 def ex_1():
     user_input = input("Please enter a word phrase, or sequence to check if it is a palindrome or not: ").lower().replace(" ","")
-    reverse_input = my_library.my_string_library.reversed_string(user_input)
+    reverse_input = my_library.my_string_library.string_reversed(user_input)
     return True if user_input == reverse_input else False
 
 def ex_2():
     user_input = float(input("Please enter a number to check if it is prime of not: "))
-    return my_library.my_library.isprime(user_input)
+    return my_library.my_library.number_isprime(user_input)
 
 def ex_3():
     user_input = int(input("Please enter a number to check if it is perfect of not: "))
-    return my_library.my_library.is_perfect_number(user_input)
+    return my_library.my_library.number_is_perfect(user_input)
+
+def ex_4():
+    user_input = input("Please enter a string: ")
+    print(f"The word set is: {my_library.my_library.string_to_words_set(user_input)}")
+    print(f"The used punctuation marks are: {my_library.my_library.string_punctuation_marks(user_input)}")
+    print(f"The most commonly used word: {my_library.my_library.dict_kex_with_max_value(my_library.my_library.string_count_number_of_times_word_is_used(user_input))}")
+    print(f"The most commonly used punctuation mark: {my_library.my_library.dict_kex_with_max_value(my_library.my_library.string_count_number_of_times_punctuation_is_used(user_input))}")
 
 if __name__ == '__main__':
     # We can store the functions as values in a dict
     exercises_map = {
         1: ex_1,
         2: ex_2,
-        3: ex_3
+        3: ex_3,
+        4: ex_4
     }
     print('Type the number of the exercise to test:')
     print('1: Palindrome exercise')
     print('2: Prime number exercise')
     print('3: Perfect number exercise')
+    print('4: String information')
     ex_nr = int(input('Exercise number: '))
-    print(exercises_map[ex_nr]())  # Executing the function at the selected number
+    if ex_nr != 4:
+        print(exercises_map[ex_nr]())  # Executing the function at the selected number
+    else:
+        exercises_map[ex_nr]()  # Executing the function at the selected number
--- my_library/my_library.py
+++ my_library/my_library.py
@@ -1,15 +1,51 @@
-def reversed_string(text: str):
+def string_reversed(text: str):
     return text[::-1]
 
-def isprime(number: float):
+def number_isprime(number: float):
     for n in range(2, int(number ** 1 / 2) + 1):
         if number % n == 0:
             return False
     return True
 
-def is_perfect_number(number: int):
+def number_is_perfect(number: int):
     sum1 = 0
     for i in range(1, number):
         if (number % i == 0):
             sum1 = sum1 + i
     return True if (sum1 == number) else False
+
+def string_to_words_set(text:str):
+    return set(string_eliminate_punctuation(text).split(" "))
+
+def string_eliminate_punctuation(text:str):
+    punctuation_marks = [".",",","!","?","-",";",":","'","(",")","[","]"]
+    for punctuation_mark in punctuation_marks:
+        text.replace(punctuation_mark, "")
+    return text
+
+def string_punctuation_marks(text:str):
+    punctuation_marks = list()
+    for letter in text:
+        if not letter.isalnum() and letter != " ":
+            punctuation_marks.append(letter)
+    return punctuation_marks
+
+def string_count_number_of_times_word_is_used(text:str):
+    word_dict = dict()
+    word_list = text.split(" ")
+    word_set = set(word_list)
+    for word in word_set:
+        word_dict[word] = word_list.count(word)
+    return word_dict
+
+def string_count_number_of_times_punctuation_is_used(text:str):
+    punctuation_mark_dict = dict()
+    punctuation_marks = string_punctuation_marks(text)
+    for punctuation_mark in punctuation_marks:
+        punctuation_mark_dict[punctuation_mark] = punctuation_marks.count(punctuation_mark)
+    return punctuation_mark_dict
+
+def dict_kex_with_max_value(my_dict:dict):
+    values_list = list(my_dict.values())
+    keys_list = list(my_dict.keys())
+    return keys_list[values_list.index(max(values_list))]
