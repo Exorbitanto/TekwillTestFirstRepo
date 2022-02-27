adc9e8e11e79f3b77c571b46f6d6926b5ad089c7
Eugen <eugen.ganta@gmail.com>
2022-02-27 17:27:42 +0200

    HW

--- HW9/ex_2.py
+++ HW9/ex_2.py
@@ -52,17 +52,19 @@ print("4: Calculate the amount of money the company has to pay in taxes per mont
 print("5: Display information for the top 10 highest paid employees (name, position, salary, employment_start_date) from highest paid to lower.")
 print("6: Display information for the top 10 employees with the longest time in the company (name, position, salary, employment_start_date) from highest to lower.")
 
-option = get_int_input("Please choose what to do (1-6) :")
-if option == 1:
-    print_list_of_values_from_list_of_dicts(data, "name")
-elif option == 2:
-    print_list_of_values_from_list_of_dicts(data, "position")
-elif option == 3:
-    print(f"Total slary amount: {calculate_total_monthly_salary(data)}")
-elif option == 4:
-    tax_rate = get_float_input("Please enter the current salary tax rate (%): ")
-    print(calculate_texes_per_month(data, tax_rate))
-elif option == 5:
-    print_highest_paid_imployees(data,10)
-elif option == 6:
-    print_longest_time_in_company_imployees(data,10)
+option = 0
+while option < 1 or option > 6:
+    option = get_int_input("Please choose what to do (1-6) :")
+    if option == 1:
+        print_list_of_values_from_list_of_dicts(data, "name")
+    elif option == 2:
+        print_list_of_values_from_list_of_dicts(data, "position")
+    elif option == 3:
+        print(f"Total slary amount: {calculate_total_monthly_salary(data)}")
+    elif option == 4:
+        tax_rate = get_float_input("Please enter the current salary tax rate (%): ")
+        print(calculate_texes_per_month(data, tax_rate))
+    elif option == 5:
+        print_highest_paid_imployees(data,10)
+    elif option == 6:
+        print_longest_time_in_company_imployees(data,10)
