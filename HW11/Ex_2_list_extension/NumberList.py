86acbe5940e2e700ed945770ed83d11ea7fb62ce
Eugen <eugen.ganta@gmail.com>
2022-03-03 18:44:02 +0200

    HW

--- HW11/Ex_2_list_extension/NumberList.py
+++ HW11/Ex_2_list_extension/NumberList.py
@@ -36,9 +36,3 @@ class NumberList(list):
             return False
 
 
-my_NumberList = NumberList([10, 22, 58, 104.23, 25.5, 24.8, 99.99])
-print(my_NumberList)
-my_NumberList.append(2022)
-print(my_NumberList)
-my_NumberList.extend([3022,4022])
-print(my_NumberList)
---
+++ HW11/Ex_2_list_extension/__init__.py
@@ -0,0 +1,8 @@
+from NumberList import NumberList
+
+my_NumberList = NumberList([10, 22, 58, 104.23, 25.5, 24.8, 99.99])
+print(my_NumberList)
+my_NumberList.append(2022)
+print(my_NumberList)
+my_NumberList.extend([3022,4022])
+print(my_NumberList)
