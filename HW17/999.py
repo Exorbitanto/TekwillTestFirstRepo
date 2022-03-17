614e3483632c751395f7cfe55afa7735b2ef8dbb
Eugen <eugen.ganta@gmail.com>
2022-03-17 21:47:53 +0200

    HW

--- HW17/999.py
+++ HW17/999.py
@@ -7,7 +7,7 @@ url_999 = 'https://999.md/ru/list/transport/cars?query={query}&sort_type=date_de
 
 def get_soup_for_query(query, nr_of_pages=None):
     for a in range(nr_of_pages):
-        time.sleep(1)
+        time.sleep(5)
         response = requests.get(
             url_999.format(query=query, page_nr=a + 1),
             headers={
@@ -16,8 +16,8 @@ def get_soup_for_query(query, nr_of_pages=None):
         yield BeautifulSoup(response.content)
 
 
-def get_soup_ad_link(ad_link):
-    response = requests.get(ad_link)
+def get_soup_for_link(link):
+    response = requests.get(link)
     return BeautifulSoup(response.content)
 
 
@@ -27,26 +27,29 @@ def get_data_for_element(element, relevant_keyword):
             return None
         image_url = element.find('div', {'class': 'ads-list-photo-item-thumb'}).find('img').get('src')
         ad_title = element.find('div', {'class': 'ads-list-photo-item-title'}).find('a').text
-        ad_link = element.find('div', {'class': 'ads-list-photo-item-title'}).find('a').get('href')
+        ad_link = f"https://999.md{element.find('div', {'class': 'ads-list-photo-item-title'}).find('a').get('href')}"
         title, year = ad_title.split(',')
         if relevant_keyword.lower() not in title.lower():
             return None
         price = element.find('div', {'class': 'ads-list-photo-item-price'}).find('span').text
-        # ad_soup = get_soup_ad_link(ad_link)
-        # user_data = get_user_data_from_soup(ad_soup)
+        ad_soup = get_soup_for_link(ad_link)
+        user_data = get_user_data_from_soup(ad_soup)
+        seller_profile_soup = get_soup_for_link(user_data["link"])
+        seller_rating = get_seller_rating_from_soup(seller_profile_soup)
 
         return dict(
             img_url=image_url,
             title=title,
             year=year,
             price=price,
-            ad_link=f"https://999.md{ad_link}"
-            # username=user_data["name"],
-            # user_link=user_data["link"]
+            ad_link=ad_link,
+            username=user_data["name"],
+            user_link=user_data["link"],
+            seller_rating=seller_rating
         )
 
-
-    except:
+    except Exception as ex:
+        print(ex)
         return None
 
 
@@ -66,6 +69,13 @@ def get_user_data_from_soup(soup):
     element_name = element.text
     return dict(name=element_name, link=element_link)
 
+def get_seller_rating_from_soup(soup):
+    element = soup.find('div', {'class': 'rating'})
+    if element:
+        return float((element.text).replace(",","."))
+    else:
+        return None
+
 
 def scrape_data(query, pages):
     all_data = []
@@ -77,4 +87,4 @@ def scrape_data(query, pages):
 
 
 if __name__ == '__main__':
-    scrape_data('BMW', 1)
+    scrape_data('BMW', 4)
